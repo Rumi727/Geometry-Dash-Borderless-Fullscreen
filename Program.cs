5216193d3d558fdf8f0d84e0a1e30e923131eb51
Rumi727 <jun32820@gmail.com>
2023-12-24 22:46:58 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Rumi.GMDBF
+{
+    public static partial class Program
+    {
+        [LibraryImport("user32.dll")] private static partial long GetWindowLongPtrA(nint hwnd, int nIndex);
+        [LibraryImport("user32.dll")] private static partial long SetWindowLongPtrA(nint hWnd, int nIndex, long dwNewLong);
+        [LibraryImport("user32.dll")] [return: MarshalAs(UnmanagedType.Bool)] private static partial bool SetWindowPos(nint hwnd, nint hwndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+        static void Main()
+        {
+            Process[] processList = Process.GetProcesses();
+            for (int i = 0; i < processList.Length; i++)
+            {
+                Process process = processList[i];
+                if (process.ProcessName == "GeometryDash")
+                    BorderlessWindow(process.MainWindowHandle);
+            }
+        }
+
+        public static void BorderlessWindow(nint handle)
+        {
+            const int GWL_STYLE = -16;
+            long curStyle = GetWindowLongPtrA(handle, GWL_STYLE);
+
+            const long WS_OVERLAPPED = 0x00000000L;
+            const long WS_CAPTION = 0x00C00000L;
+            const long WS_MINIMIZEBOX = 0x00020000L;
+            const long WS_MAXIMIZEBOX = 0x00010000L;
+            const long WS_SYSMENU = 0x00080000L;
+            const long WS_MAXIMIZE = 0x01000000L;
+            const long WS_THICKFRAME = 0x00040000L;
+            const long WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+
+            curStyle &= ~WS_OVERLAPPEDWINDOW;
+            curStyle |= WS_MAXIMIZE;
+
+            SetWindowLongPtrA(handle, GWL_STYLE, curStyle);
+
+            const int SWP_FRAMECHANGED = 0x0020;
+            const int SWP_SHOWWINDOW = 0x0040;
+            SetWindowPos(handle, 0, 0, 0, 1280, 720, SWP_FRAMECHANGED | SWP_SHOWWINDOW);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# Geometry Dash Borderless Fullscreen
+
+WinAPI를 사용하여 지메를 테두리 없는 전체화면으로 만들어주는 2% 부족한 프로그램입니다.
+This is a 2% lacking program that uses WinAPI to make Geometry Dash borderless full screen.
+
+## 적용 방법 (How to apply)
+
+1. 지메 실행
+2. 전체화면 끄기
+3. 프로그램 실행
+4. 끝!
+
+참고: 지메 ㅈ망겜이라 그 어떤 짓을 해도 제 실력으로는 비율을 완전히 16:9로 만들지 못했습니다.  
+
+---
+
+1. Run Geometry Dash
+2. Turn off full screen
+3. Run program
+4. End!
+
+Note: Geometry Dash is a shit game, so I tried a lot, but I can't make the ratio completely 16:9 with my skills.
+
+## 작동 원리 (How it Works)
+
+1. 모든 프로세스 목록을 구한 후, 'GeometryDash'라는 이름을 가진 프로세스를 찾습니다. (Process.GetProcesses())
+2. 가져온 프로세스의 메인 창의 핸들을 구합니다. (Process.MainWindowHandle)
+3. user32.dll 함수를 사용하여 가져온 창의 핸들의 속성 값을 변경합니다 (SetWindowLongPtrA())
+4. 적용한 속성을 창에 적용시킵니다 (SetWindowPos())
+
+---
+
+1. After getting a list of all processes, find the process with the name ‘GeometryDash’. (Process. GetProcesses())
+2. Obtain the handle of the main window of the imported process. (Process. MainWindowHandle)
+3. Change the property value of the handle of the imported window using the user32.dll function (SetWindowLongPtrA())
+4. Apply the applied properties to the window (SetWindowPos())
